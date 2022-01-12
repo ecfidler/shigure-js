da12a2e7601a15699a7070852cdf9caa4cb813ac
Ethan Fidler <ethancf1999@gmail.com>
2022-01-11 21:32:36 -0500

    thanks brendan :)

--- commands/echo.js
+++ commands/echo.js
@@ -7,8 +7,8 @@ const commandData = {
     defaultPermission: false,
     permissions : [
         {
-            id: ROLES.MAJOR,
-            type: "ROLE",
+            id: OWNER,
+            type: "USER",
             permission: true,
         }
     ],
@@ -24,14 +24,8 @@ const commandData = {
 };
 
 async function action(client, interaction) {
-    if (interaction.user.id != OWNER) {
-        await interaction.reply({content: "this command is set to owner only, sorry ben", ephemeral: true});
-    }
-    else {
-        await interaction.reply({content: "message echoed!", ephemeral: true});
-        await interaction.channel.send({ content: interaction.options.get("content").value});
-    }
-    
+    await interaction.reply({content: "message echoed!", ephemeral: true});
+    await interaction.channel.send({ content: interaction.options.get("content").value});
 }
 
 module.exports = {commandData, action, guild};
