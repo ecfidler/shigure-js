3ccc4ad115f9baae1bdae24d63374de1817c5d07
Ethan Fidler <ethancf1999@gmail.com>
2025-03-06 12:06:06 -0500

    good friends!

--- utilities/constants.js
+++ utilities/constants.js
@@ -43,7 +43,7 @@ const MAXIMUM_BUTTON_ROWS = 5;
 /**
  * Regex of characters not allowed in emoji names, such as spaces or periods.
  */
-const DISALLOWED_EMOJI_CHARACTERS_REGEX = /[ .]/g;
+const DISALLOWED_EMOJI_CHARACTERS_REGEX = /[ !?.]/g;
 
 module.exports = {
     GUILDS,
