4e79ef7ba2b23b57f88233b65a652f24c75de1a3
Ben Rucker <brucker117@gmail.com>
2025-04-26 17:41:11 -0700

    Install TS

---
+++ src/events/changeRolesPage.ts
@@ -0,0 +1,22 @@
+const { getButtonRows, getRoles } = require("../utilities/roleup.js");
+
+const { MessageFlags } = require("discord.js");
+
+async function changeRolesPageEvent(client, interaction) {
+    const [category, pageNumber] = interaction.customId.split("_").slice(1);
+
+    const components = getButtonRows(
+        await getRoles(client, category, interaction.guild),
+        interaction.member,
+        category,
+        parseInt(pageNumber)
+    );
+
+    await interaction.update({
+        embeds: interaction.embeds,
+        components: components,
+        flags: MessageFlags.Ephemeral,
+    });
+}
+
+module.exports = { changeRolesPageEvent };
